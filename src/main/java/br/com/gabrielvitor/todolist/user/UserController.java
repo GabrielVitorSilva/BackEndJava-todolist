324511a95aeca1e5a0a757b30788e9cb1d2502aa
GabrielVitorSilva <gabrieldatas2004@gmail.com>
2023-10-10 12:05:24 -0300

    feat: created database connection and route to creat users

--- src/main/java/br/com/gabrielvitor/todolist/user/UserController.java
+++ src/main/java/br/com/gabrielvitor/todolist/user/UserController.java
@@ -1,5 +1,8 @@
 package br.com.gabrielvitor.todolist.user;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -8,9 +11,18 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/users")
 public class UserController {
+
+  @Autowired
+  private IUserRepository userRepository;
   
   @PostMapping("/")
-  public UserModel create(@RequestBody UserModel userModel){
-    return userModel;
+  public ResponseEntity create(@RequestBody UserModel userModel){
+    var user = this.userRepository.findByUsername(userModel.getUsername());
+
+    if(user != null){
+      return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("User already exists");
+    }
+    var userCreated = this.userRepository.save(userModel);
+    return ResponseEntity.status(HttpStatus.CREATED).body(userCreated);
   }
 }
